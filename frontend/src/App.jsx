b2d928c5437fa10d068d3ca362617cc3fa84776c
habibasmit <habibakaleem525@gmail.com>
2025-01-25 04:22:42 +0500

    checking its working or not after deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className='bg-gray-100 min-h-screen'>
       <h1 className='font-bold text-center'>Hackathon</h1>
+      <p className='text-2xl font-semibold text-green-500'>checking its working or not after deployment or submition the task</p>
     </div>
   )
 }
